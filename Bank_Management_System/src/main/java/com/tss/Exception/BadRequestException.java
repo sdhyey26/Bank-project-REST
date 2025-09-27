e65d31ea57c4329d3b256bcfc672eb598cc6f8e1
Dhyey Shiyal <147261942+sdhyey26@users.noreply.github.com>
2025-09-27 10:31:06 +0530

    Delete Bank_Management_System directory

--- Bank_Management_System/src/main/java/com/tss/Exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.tss.Exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
-
-
--- Bank_Management_System/src/main/java/com/tss/Exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tss.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
-
